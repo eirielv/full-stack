c4d155723f69b2f043ae2ce4e1df1b61fc87d481
eirikelv <eirik.org@gmail.com>
2023-01-30 23:22:23 +0100

    feat: content from starting up a Vue project added
    content from WebStorm

---
+++ Oving2/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Calculator from './components/Calculator.vue'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+        header
+      </div>
+    </header>
+
+    <main>
+      <Calculator/>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+#layout{
+  justify-content: center;
+}
+</style>
