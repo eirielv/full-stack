96fc9e857168c1d70eebeb9848c360944081d1e3
eirikelv <eirik.org@gmail.com>
2023-02-03 16:06:36 +0100

    feat: add store to main.js implementation

--- Oving2/src/main.js
+++ Oving2/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
+import store from "@/store";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .mount('#app')
